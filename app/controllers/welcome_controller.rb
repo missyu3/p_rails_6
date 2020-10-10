7df80288f05a7ca92e3d0acf1065ef51b0ecd9b4
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-10-10 22:36:13 +0900

    Fix ルートの設定とコントローラクラスが不一致のため修正

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,4 @@
 class WelcomeController < ApplicationController
   def index
   end
-  def create
-    user = User.find_or_create_from_auth_hash!(request.env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_path, notice: "ログインしました"
-  end
 end
